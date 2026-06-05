af7e985970cc031621214a63d17ce9d01b6a982c
Girishma13 <girishmagiri13@gmail.com>
2026-06-05 10:55:27 +0530

    Implement logging for requests and responses

--- logging middleware/LogFilter.java
+++ logging middleware/LogFilter.java
@@ -1,1 +1,17 @@
+package logging_middleware;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LogFilter {
+
+    public void logRequest(String method, String url) {
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        System.out.printf("[%s] INFO: Request -> Method: %s | URL: %s\n", timestamp, method, url);
+    }
+
+    public void logResponse(String url, int status) {
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        System.out.printf("[%s] INFO: Response <- URL: %s | Status: %d\n", timestamp, url, status);
+    }
+}
